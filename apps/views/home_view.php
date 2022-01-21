bd88856a01595529dcbbd28cc10775626071b38e
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2022-01-21 23:40:40 +0700

    feat: switch chart 2 and 3 position

--- apps/views/home_view.php
+++ apps/views/home_view.php
@@ -7,7 +7,7 @@ use MiniMvc\Apps\Core\Bootstraping\Security;
 <?php require('partials/header.php'); ?>
 
 <body>
-  <div class="wrapper flex">
+  <div class="flex">
     <div class="charts">
       <div class="chart-container item1" style="position: relative; height: 40vh; width: 45vw">
         <canvas id="myChart1"></canvas>
@@ -62,54 +62,9 @@ use MiniMvc\Apps\Core\Bootstraping\Security;
       },
     });
 
-    const myChart2 = new Chart(ctx2, {
-      type: 'bar',
-      data: {
-        labels: [
-          <?php foreach ($docteratas as $data) : ?> '<?php echo $data->nama_dosen ?>',
-          <?php endforeach; ?>
-        ],
-        datasets: [{
-            label: 'Jumlah Dokumen',
-            data: [<?php foreach ($docteratas as $data) : ?> '<?php echo $data->jumlah ?>',
-              <?php endforeach; ?>
-            ],
-            backgroundColor: ['rgba(75, 192, 192, 0.2)'],
-            borderColor: ['rgba(75, 192, 192, 1)'],
-            borderWidth: 1,
-          },
-          {
-            label: 'Jumlah Sitasi',
-            data: [<?php foreach ($docsitasiteratas as $data) : ?> '<?php echo $data->jumlah ?>',
-              <?php endforeach; ?>
-            ],
-            backgroundColor: ['rgba(153, 102, 255, 0.2)'],
-            borderColor: ['rgba(153, 102, 255, 1)'],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        interaction: {
-          intersect: false,
-          mode: 'index',
-        },
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: '10 Dosen Dengan Publikasi Terbanyak',
-          },
-        },
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
 
-    const myChart3 = new Chart(ctx3, {
+
+    const myChart2 = new Chart(ctx2, {
       type: 'bar',
       data: {
         labels: [
@@ -142,12 +97,58 @@ use MiniMvc\Apps\Core\Bootstraping\Security;
           mode: 'index',
         },
         responsive: true,
-        aspectRatio: 13 / 3,
-
         plugins: {
           title: {
             display: true,
             text: '10 Prodi Dengan Dokumen Publikasi Terbanyak',
+          },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    const myChart3 = new Chart(ctx3, {
+      type: 'bar',
+      data: {
+        labels: [
+          <?php foreach ($docteratas as $data) : ?> '<?php echo $data->nama_dosen ?>',
+          <?php endforeach; ?>
+        ],
+        datasets: [{
+            label: 'Jumlah Dokumen',
+            data: [<?php foreach ($docteratas as $data) : ?> '<?php echo $data->jumlah ?>',
+              <?php endforeach; ?>
+            ],
+            backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+            borderColor: ['rgba(75, 192, 192, 1)'],
+            borderWidth: 1,
+          },
+          {
+            label: 'Jumlah Sitasi',
+            data: [<?php foreach ($docsitasiteratas as $data) : ?> '<?php echo $data->jumlah ?>',
+              <?php endforeach; ?>
+            ],
+            backgroundColor: ['rgba(153, 102, 255, 0.2)'],
+            borderColor: ['rgba(153, 102, 255, 1)'],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        interaction: {
+          intersect: false,
+          mode: 'index',
+        },
+        responsive: true,
+        aspectRatio: 13 / 3,
+        plugins: {
+          title: {
+            display: true,
+            text: '10 Dosen Dengan Publikasi Terbanyak',
           },
         },
         scales: {
